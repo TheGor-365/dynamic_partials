724338c4117f6174280a9267b235947464b767c2
Gor <7495500@gmail.com>
2024-02-20 23:55:25 +0300

    Add partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'pages/home',    to: 'pages#home'
+  get 'pages/about',   to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
 end
